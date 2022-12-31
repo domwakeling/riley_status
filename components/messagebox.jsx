f66447116a4d3c4aaeac3967b614c7809ec4d3a3
Dom Wakeling <domwakeling@gmail.com>
2022-12-31 11:37:24 +0000

    Messages working, other than fod

--- components/messagebox.jsx
+++ components/messagebox.jsx
@@ -1,8 +1,47 @@
 const MessageBox = ({ isLoading, data, date, threshold }) => {
 
+    let oldDate = null;
+    const newDate = new Date();
+    let daysAgo = 0
+    let message = "";
+
+    if (!isLoading) {
+        const m = data[date].match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})/);
+        oldDate = new Date();
+        oldDate.setDate(m[1]);
+        oldDate.setMonth(parseInt(m[2] - 1));
+        oldDate.setFullYear(m[3]);
+        oldDate.setHours(0);
+        oldDate.setMinutes(0);
+        oldDate.setSeconds(0);
+
+        newDate.setHours(0);
+        newDate.setMinutes(0);
+        newDate.setSeconds(0);
+    }
+
+    if (oldDate) {
+        daysAgo = Math.floor( (newDate.getTime() - oldDate.getTime() + 60 * 60 * 1000) / ( 24 * 60 * 60 * 1000) );
+        if (daysAgo == 0) {
+            message = "today";
+        } else if (daysAgo <= 10) {
+            message = `${daysAgo} day${daysAgo > 1 ? 's' : ''} ago`;
+        } else if (daysAgo <= 35) {
+            const weeksAgo = Math.floor(daysAgo / 7);
+            message = `${weeksAgo} week${weeksAgo > 1 ? 's' : ''} ago`;
+        } else {
+            const monthsAgo = Math.floor(daysAgo / 30);
+            message = `${monthsAgo} month${monthsAgo > 1 ? 's' : ''} ago`;
+        }
+    } else {
+        message = '...';
+    }
+
+    const classToAdd = (daysAgo >= threshold ? 'overdue' : 'timely')
+
     return (
-        <p>
-            {isLoading ? '...' : data[date]}
+        <p className={classToAdd}>
+            {isLoading ? '...' : message}
         </p>
     )
 }
